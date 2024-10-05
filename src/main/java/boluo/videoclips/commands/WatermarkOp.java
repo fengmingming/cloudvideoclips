94181947820b65c63b2e2b7f04c8b2c875b7db21
fengmingming <1053931671@qq.com>
2024-10-05 12:07:18 +0800

    watermark

--- src/main/java/boluo/videoclips/commands/WatermarkOp.java
+++ src/main/java/boluo/videoclips/commands/WatermarkOp.java
@@ -1,36 +1,78 @@
 package boluo.videoclips.commands;
 
 import boluo.videoclips.OpChain;
+import cn.hutool.core.util.StrUtil;
 import lombok.Getter;
 import lombok.Setter;
 import org.bytedeco.javacv.Frame;
 import org.bytedeco.javacv.Java2DFrameUtils;
 
+import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.net.URL;
 
-@Setter
-@Getter
 public class WatermarkOp extends Op{
 
-    private String fontName;
-    private int fontType = Font.PLAIN;
-    private int fontSize = 12;
-    private String text;
-    private int x;
-    private int y;
+    private @Setter @Getter String fontName;
+    private @Setter @Getter int fontType = Font.PLAIN;
+    private @Setter @Getter int fontSize = 22;
+    private @Setter @Getter String text;
+    private @Setter @Getter String imageUrl;
+    private @Setter @Getter int x = 50;
+    private @Setter @Getter int y = 50;
+    private @Setter @Getter int r = 255;
+    private @Setter @Getter int g = 255;
+    private @Setter @Getter int b = 255;
+    private BufferedImage image;
+
+    public void start() {
+        if(x < 0) {
+            x = 50;
+        }
+        if(y < 0) {
+            y = 50;
+        }
+        if(r < 0 || r > 255) {
+            r = 255;
+        }
+        if(g < 0 || g > 255) {
+            g = 255;
+        }
+        if(b < 0 || b > 255) {
+            b = 255;
+        }
+        if(StrUtil.isNotBlank(imageUrl)) {
+            try {
+                image = ImageIO.read(new URL(imageUrl));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
 
     @Override
     public void doFilter(OpContext context, Frame frame, OpChain chain) {
         //视频帧才处理
         if(frame.image != null) {
-            BufferedImage image = Java2DFrameUtils.toBufferedImage(frame);
-            Graphics2D g = image.createGraphics();
-            g.setFont(new Font(fontName, fontType, fontSize));
-            g.drawString(getText(), getX(), getY());
-            g.dispose();
-            frame = Java2DFrameUtils.toFrame(image);
+            int x = getX();
+            int y = getY();
+            if(x < frame.imageWidth && y < frame.imageHeight && (StrUtil.isNotBlank(getText()) || this.image != null)) {
+                BufferedImage bi = Java2DFrameUtils.toBufferedImage(frame);
+                Graphics2D g2D = bi.createGraphics();
+                if(StrUtil.isNotBlank(getText())) {
+                    g2D.setFont(new Font(fontName, fontType, fontSize));
+                    g2D.setColor(new Color(r,g,b));
+                    g2D.drawString(getText(), x, y);
+                }else {
+                    g2D.drawImage(this.image, x, y, null);
+                }
+                g2D.dispose();
+                frame = Java2DFrameUtils.toFrame(bi);
+            }
         }
+        chain.doFilter(context, frame);
     }
 
 }
--- src/test/java/boluo/videoclips/TestVideoClips.java
+++ src/test/java/boluo/videoclips/TestVideoClips.java
@@ -3,6 +3,7 @@ package boluo.videoclips;
 import boluo.Bootstrap;
 import boluo.videoclips.commands.TrimOp;
 import boluo.videoclips.commands.VideoClipsCommand;
+import boluo.videoclips.commands.WatermarkOp;
 import jakarta.annotation.Resource;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -33,8 +34,19 @@ public class TestVideoClips {
         clipService.videoClip(command);
     }
 
+    @Test
     public void testWaterMark() {
-        
+        VideoClipsCommand command = new VideoClipsCommand();
+        WatermarkOp op = new WatermarkOp();
+        op.setImageUrl("https://docbook.com.cn/_nuxt/img/docbook-header-logo.ab2d9eb.png");
+        op.setFontSize(66);
+        op.setR(255);
+        op.setG(255);
+        op.setB(255);
+        command.setOps(List.of(op));
+        command.setUrl("https://testv.docbook.com.cn/customerTrans/692bec965a43d406e8ad68dee453a2f5/639f789f-188c70bbb4f-0006-e012-d30-d3e2c.mp4");
+        command.setTargetUrls(List.of("/vc_watermark.mp4"));
+        clipService.videoClip(command);
     }
 
 }
